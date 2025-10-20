5f4e88ef6e19c48696fd7c98d2a024937888489e
eadewusiCOS101 <eyinoluwa.adewusi@pau.edu.ng>
2025-10-20 11:31:21 +0100

    week 4 commit

---
+++ week-4/practice-1/src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+fn main() {
+    println!("\nStudent Information Management System.");
+
+    println!("\nPlease enter your name.");
+    let mut name = String::new();
+        io::stdin()
+        .read_line(&mut name)
+        .expect("failed to read input!");
+    println!("So, your name is {}", name);
+
+    println!("Alright {}, how about your age?", name);
+    let mut age = String::new();
+        io::stdin().read_line(&mut age).expect("Failed to read input");//This reads what is being written in the input.
+    let age:i32 = age.trim().parse().expect("Input is not valid");
+    println!("You are {} years old.", age);
+}
