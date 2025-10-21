fdd6c2588d73b808cc06c1e959a0b91f4148b25d
eadewusiCOS101 <eyinoluwa.adewusi@pau.edu.ng>
2025-10-21 12:46:18 +0100

    week 4 commit

--- week-4/project-2/src/main.rs
+++ week-4/project-2/src/main.rs
@@ -1,26 +1,38 @@
 use std::io;
 
 fn main() {
-    println!("Are you Experienced? y or n.");
-    let mut input1 = String::new();
-    io::stdin().read_line(&mut input1).expect("Invalid input.");
-    let exp:char = input1.trim().parse().expect("Invalid input.");
+    let mut stat:bool = true; 
 
-    println!("Enter your age.");
-    let mut input2 = String::new();
-    io::stdin().read_line(&mut input2).expect("Invalid input.");
-    let age:u32 = input2.trim().parse().expect("Invalid input.");
+    while stat == true{
 
-    if exp == 'y' && age >= 40{
-        println!("Your incentive is NGN1,560,000");
+        println!("Are you Experienced? y or n.");
+        let mut input1 = String::new();
+        io::stdin().read_line(&mut input1).expect("Invalid input.");
+        let exp:char = input1.trim().parse().expect("Invalid input.");
+
+        if exp != 'y' || exp != 'n'{
+            println!("Invalid input.");
+            break;
+        }
+
+        println!("Enter your age.");
+        let mut input2 = String::new();
+        io::stdin().read_line(&mut input2).expect("Invalid input.");
+        let age:u32 = input2.trim().parse().expect("Invalid input.");
+
+        if exp == 'y' && age >= 40{
+            println!("Your incentive is NGN1,560,000");
     }
-    else if exp == 'y' && age >= 30 && age < 40{
-        println!("Your incentive is NGN1,480,000");
+        else if exp == 'y' && age >= 30 && age < 40{
+            println!("Your incentive is NGN1,480,000");
     } 
-    else if exp == 'y' && age < 28{
-        println!("Your incentive is NGN1,300,000");
+        else if exp == 'y' && age < 28{
+            println!("Your incentive is NGN1,300,000");
     }
-    else{
-        println!("Your incentive is NGN100,000");
+        else{
+            println!("Your incentive is NGN100,000"); 
+
+        }
     }
+    
 }
