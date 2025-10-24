adbcc62e50d4a8de5d01be4d064a4e0233db6a61
eadewusiCOS101 <eyinoluwa.adewusi@pau.edu.ng>
2025-10-24 10:45:20 +0100

    Week 4 commit

--- week-4/project-2/src/main.rs
+++ week-4/project-2/src/main.rs
@@ -12,38 +12,38 @@ fn main() {
         io::stdin().read_line(&mut input1).expect("Invalid input");
         let exp : &str = input1.trim();
 
-        if exp == "y" || exp == "Y"{
-            println!("You are experienced.");
-        }
-        else if exp == "n" || exp == "N"{
-            println!("You are inexperienced.");
-        }
-        else{
-            println!("Invalid input.");
-            break;
-        }
-
-        
         println!("How old are you?");
         io::stdin().read_line(&mut input2).expect("Invalid input.");
         let age:u16 = input2.trim().parse().expect("Invalid input.");
 
 
-        if exp == "y" || exp == "Y" && age >= 40{
-            println!("Your incentive is NGN1,560,000.00");
-            
-        } 
-        else if exp == "y" || exp == "Y" && age >=30 && age < 40{
-            println!("Your incentive is NGN1,480,000.00");
+        if exp == "y" || exp == "Y"{
+            let exp_status = "You are experienced.";
+            println!("{}", exp_status);
+            if exp_status == "You are experienced" && age >= 40{
+                println!("Your incentive is NGN1,560,000.00");
+            }
+
+            else if exp_status == "You are experienced" && age >=30 && age < 40{
+                println!("Your incentive is NGN1,480,000.00");
+            }
+
+            else{
+                println!("Your incentive is NGN1,300,000.00");
+            }
         }
-        else if exp == "y" || exp == "Y" && age <28{
-            println!("Your incentive is NGN1,300,000.00");
-        }
-        else{
+
+        else if exp == "n" || exp == "N"{
+            let exp_status = "You are inexperienced.";
+            println!("{}", exp_status);
             println!("Your incentive is NGN100,000.00");
         }
 
-        stat -= 1;
+        else{
+            println!("Invalid input.");
+            break;
+        }
 
+        stat -= 1;
     }
 }
