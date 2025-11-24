69f774fa8a0ecbb5c390d51da154b774eb2b8000
eadewusiCOS101 <eyinoluwa.adewusi@pau.edu.ng>
2025-11-24 20:29:09 +0100

    Week 9 commit

--- week-9/project-3/src/main.rs
+++ week-9/project-3/src/main.rs
@@ -2,20 +2,35 @@ use std::fs::File;
 use std::io::Write;
 
 fn main(){
-    let data = vec![
-    ("Aigbogun Alamba Daudu","Internal Affairs","South West"),
-    ("Murutala Afeez Bendu","Justice","North East"),
-    ("Okorocha Calistus Ogbonga","Defense","South South"),
-    ("Adewale Jimoh Akanbi","Power and Steel","South West"),
-    ("Osazuwa Faith Eiteye","Petroleum","South East")];
+    let commisioner_name = vec![
+    "Aigbogun Alamba Daudu      ",
+    "Murutala Afeez Bendu       ",
+    "Okorocha Calistus Ogbonga  ",
+    "Adewale Jimoh Akanbi       ",
+    "Osazuwa Faith Eiteye       "];
+
+    let commisioner_ministry = vec![
+    "Internal Affairs   ",
+    "Justice            ",
+    "Defense            ",
+    "Power and Steel    ",
+    "Petroleum          "];
+
+    let commisioner_gpz = vec![
+    "South West ",
+    "North East ",
+    "South South",
+    "South West ",
+    "South East "];
 
     let mut file = File::create("Commisioner info.txt").expect("Unable to create file.");
     file.write_all("All Commisioner information extracted from the backup.\n
-Order of information mention: Name, Ministry, Geopolictical Zone.\n".as_bytes()).expect("Could not complete");
+ Name                           Ministry      Geopolictical Zone.\n".as_bytes()).expect("Could not complete");
 
-    for commisioner in 0..data.len(){
-        let line = format!("{:?}\n",data[commisioner]);
+    for c in 0..commisioner_name.len(){
+        let line = format!("{}{}{}\n",commisioner_name[c], commisioner_ministry[c],commisioner_gpz[c]);
         file.write_all(line.as_bytes()).expect("Error occured");
     }
     println!("Information Written successfully into the file!");
+    
 }
